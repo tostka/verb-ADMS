54c116faa4f31f6d0774f19d181dce75b22f5ca3
Tostka <tostka@users.noreply.github.com>
2020-04-01 16:44:40 -0500

    verb-adms
    4:43 PM 4/1/2020 rebuilt

--- Tests/verb-ADMS.tests.ps1
+++ Tests/verb-ADMS.tests.ps1
@@ -162,7 +162,7 @@ Describe 'Module Information' -Tags 'Command'{
             $Script:ManifestHash.RootModule | Should -Be "$ModuleName.psm1"
         }
         It 'Valid Manifest GUID' {
-            $Script:ManifestHash.Guid | Should -Be "b9637e55-12be-4916-8000-a949f9426fa3"
+            $Script:ManifestHash.Guid | Should -Be "1fda24ae-c9e2-4154-b58d-4910731be1a4"
         }
         <#
         It 'No Format File' {
@@ -382,3 +382,29 @@ $AnalyzerIssues | export-clixml -path $ofile
 write-verbose -verbose:$verbose  "ScriptAnalyzer Report written to:`n$($ofile)" ;
 
 #endregion
+
+# SIG # Begin signature block
+# MIIELgYJKoZIhvcNAQcCoIIEHzCCBBsCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUXxFffwF3FhD3lLNoPs8P5+1z
+# y6+gggI4MIICNDCCAaGgAwIBAgIQWsnStFUuSIVNR8uhNSlE6TAJBgUrDgMCHQUA
+# MCwxKjAoBgNVBAMTIVBvd2VyU2hlbGwgTG9jYWwgQ2VydGlmaWNhdGUgUm9vdDAe
+# Fw0xNDEyMjkxNzA3MzNaFw0zOTEyMzEyMzU5NTlaMBUxEzARBgNVBAMTClRvZGRT
+# ZWxmSUkwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBALqRVt7uNweTkZZ+16QG
+# a+NnFYNRPPa8Bnm071ohGe27jNWKPVUbDfd0OY2sqCBQCEFVb5pqcIECRRnlhN5H
+# +EEJmm2x9AU0uS7IHxHeUo8fkW4vm49adkat5gAoOZOwbuNntBOAJy9LCyNs4F1I
+# KKphP3TyDwe8XqsEVwB2m9FPAgMBAAGjdjB0MBMGA1UdJQQMMAoGCCsGAQUFBwMD
+# MF0GA1UdAQRWMFSAEL95r+Rh65kgqZl+tgchMuKhLjAsMSowKAYDVQQDEyFQb3dl
+# clNoZWxsIExvY2FsIENlcnRpZmljYXRlIFJvb3SCEGwiXbeZNci7Rxiz/r43gVsw
+# CQYFKw4DAh0FAAOBgQB6ECSnXHUs7/bCr6Z556K6IDJNWsccjcV89fHA/zKMX0w0
+# 6NefCtxas/QHUA9mS87HRHLzKjFqweA3BnQ5lr5mPDlho8U90Nvtpj58G9I5SPUg
+# CspNr5jEHOL5EdJFBIv3zI2jQ8TPbFGC0Cz72+4oYzSxWpftNX41MmEsZkMaADGC
+# AWAwggFcAgEBMEAwLDEqMCgGA1UEAxMhUG93ZXJTaGVsbCBMb2NhbCBDZXJ0aWZp
+# Y2F0ZSBSb290AhBaydK0VS5IhU1Hy6E1KUTpMAkGBSsOAwIaBQCgeDAYBgorBgEE
+# AYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwG
+# CisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBRbHO5m
+# h57bFJm10BNPsP/Y7r2ArDANBgkqhkiG9w0BAQEFAASBgCOgonGmVrLsI/7JkV7G
+# 6aag5PQFt7SewMgqUmuYkBHmvcdSWA4RLN/bWnCAsYKRVwLkJ6j94YAO2mcuc+r1
+# gNlpND89wFYgtVJvkjyG4JcsCtwAtsWzrii/+oV65ANeHQVXzTIVVmYWocaCd637
+# Mso145pSm6N0RWm0QEfAGU6l
+# SIG # End signature block
