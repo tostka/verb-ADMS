6d79e47494d6be7cc7addb59ce1a97e3eaba13f2
Tostka <tostka@users.noreply.github.com>
2021-11-30 14:40:14 -0600

    Update grant-ADGroupManagerUpdateMembership.ps1
    * 2:08 PM 11/30/2021 validated functional ; pulled verb-adms req, added alt to use get-addomaincontroller if verb-adms:get-gcfast() isn't avail

--- Public/grant-ADGroupManagerUpdateMembership.ps1
+++ Public/grant-ADGroupManagerUpdateMembership.ps1
@@ -15,9 +15,19 @@ function grant-ADGroupManagerUpdateMembership {
     Github      : https://github.com/tostka/verb-adms
     Tags        : Powershell,ActiveDirectory,Permissions,Groups
     REVISIONS
+    * 2:08 PM 11/30/2021 validated functional ; pulled verb-adms req, added alt to use get-addomaincontroller if verb-adms:get-gcfast() isn't avail
     * 3:18 PM 11/29/2021 initial vers
     .DESCRIPTION
     grant-ADGroupManagerUpdateMembership - For a specified ADGroup, add '[x]Manager can update membership list' (visible in ADUC on the gorup's Managed-By tab)
+    This function is an alternative *ADMS*-native powershell approach, to the standard Exchange Mgmt Shell option:    
+    #-=-=-=-=-=-=-=-=
+    $grp=Get-DistributionGroup -id $group -domainc $dc;
+    $pltAADP=[ordered]@{ User=$Manager ; AccessRights='WriteProperty' ; Properties='Member' ; domaincontroller=$dc ; ErrorAction = 'STOP' ; whatif=$($whatif) ; } ;
+    $grp | Add-ADPermission @pltAADP ;
+    $ADPerms = $grp | Get-ADPermission -domainc $dc | ? {($_.Properties -match 'Member') -AND ($_.AccessRights -eq 'WriteProperty') -AND ($_.User -notlike 'AD\Exchange*')} ; 
+    #-=-=-=-=-=-=-=-=
+    (which throsw cryptic Access Denied errors, in my enviro, and I don't have time to chase undocumented low level AD perm errors). 
+    For me, the ADMS approach works either way. I'm moving ahead on the subject. 
     .PARAMETER  User
     Samaccountname/GUID/DN for user to be granted ManagedBy and 'Manager can update membership list'
     .PARAMETER  group
@@ -29,6 +39,7 @@ function grant-ADGroupManagerUpdateMembership {
     Configure the permissions for 'someuser' on the 'group X' group, with verbose output
     .LINK
     #>
+    #Requires -Modules ActiveDirectory, verb-ADMS
     # VALIDATORS: [ValidateNotNull()][ValidateNotNullOrEmpty()][ValidateLength(24,25)][ValidateLength(5)][ValidatePattern("(lyn|bcc|spb|adl)ms6(4|5)(0|1).(china|global)\.ad\.toro\.com")][ValidateSet("USEA","GBMK","AUSYD")][ValidateScript({Test-Path $_ -PathType 'Container'})][ValidateScript({Test-Path $_})][ValidateRange(21,65)][ValidateCount(1,3)]
 ## [OutputType('bool')] # optional specified output type
 
@@ -38,7 +49,9 @@ function grant-ADGroupManagerUpdateMembership {
         ##[Alias('XXX')]
         [string]$User,
         [parameter(Mandatory=$True,HelpMessage="AD identifyer (name,DN,guid) for ADGroup/DistribGroup to be configured with the updated permission")]
-        [string]$group,        
+        [string]$group,
+        [Parameter(HelpMessage="Switch to return the ACL object to the pipeline (rather than `$true/`$false)[-ReturnObject]")]
+        [switch] $ReturnObject,
         [Parameter(HelpMessage="Whatif switch [-whatIf]")]
         [switch] $whatIf
     ) ;  
@@ -52,7 +65,14 @@ function grant-ADGroupManagerUpdateMembership {
     
     $error.clear() ;
     TRY {
-        $dc = get-gcfast -Verbose:$($VerbosePreference -eq "Continue") ;         
+        if(get-command get-gcfast){
+            # use my verb-adms mod function if avail (validates speed)
+            $dc = get-gcfast -Verbose:$($VerbosePreference -eq "Continue") ;         
+        } else { 
+            # use default adms option
+            #$dc = (GET-ADDomaincontroller -discover).hostname
+            $dc = (Get-ADDomainController -Filter  {isGlobalCatalog -eq $true -AND Site -eq "$((get-adreplicationsite).name)"}).hostname| Get-Random ; 
+        } ; 
         $pltGADU=[ordered]@{
             identity = $user ;
             server=$dc; ErrorAction='STOP'; 
@@ -132,6 +152,7 @@ function grant-ADGroupManagerUpdateMembership {
                 else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
             
                 Set-Acl @pltSACL ; 
+                if(-not $whatif){
                 $acl = Get-Acl $aclPath ;
                 if($grantACL = ($acl).Access |?{$_.objecttype -eq $SelfMembershipGuid -AND $_.InheritanceType -eq 'None' -AND $_.IdentityReference -eq "$($domNBName)\$($grantee.SamAccountName)"}){ 
                     $smsg = "Updated matching ACL w`n$(($grantACL | fl $propsACL|out-string).trim())" ; 
@@ -142,14 +163,32 @@ function grant-ADGroupManagerUpdateMembership {
                     $smsg += "`n| ?{$_.objecttype -eq $SelfMembershipGuid -AND $_.InheritanceType -eq 'None' -AND $_.IdentityReference -eq '$($domNBName)\$($grantee.SamAccountName)'} ; "
                     if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN } #Error|Warn|Debug 
                     else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; 
-
+                } ; 
+                } else { 
+                    $smsg = "(-whatif: skipping verifications)" ; 
+                    if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
+                    else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
                 } ; 
             } ; 
         } else { 
-            throw "Missing 'AD:' PsDrive!" 
+            $smsg = "Missing 'AD:' PsDrive!" 
+            if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level warn } #Error|Warn|Debug 
+            else{ write-warning "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; 
+            throw $smsg ;
         } ; 
 
-        $true | write-output ; 
+        if($ReturnObject){
+            $smsg = "(-ReturnObject specified: returning applied ACL object to pipeline)" ; 
+            if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
+            else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; 
+            $grantACL | write-output ; 
+        } else { 
+            $smsg = "(returning `$true to pipeline)" ; 
+            if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
+            else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; 
+            $true | write-output ; 
+        } ; 
+
     } CATCH {
         $ErrTrapd=$Error[0] ;
         $smsg = "$('*'*5)`nFailed processing $($ErrTrapd.Exception.ItemName). `nError Message: $($ErrTrapd.Exception.Message)`nError Details: `n$(($ErrTrapd|out-string).trim())`n$('-'*5)" ;
