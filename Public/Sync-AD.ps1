2dd9ec384403567f7459c10af8308eb6068b51a5
Tostka <tostka@users.noreply.github.com>
2024-11-21 15:47:41 -0600

    Update Sync-AD.ps1
    3:47 PM 11/21/2024

--- Public/Sync-AD.ps1
+++ Public/Sync-AD.ps1
@@ -1,8 +1,29 @@
 ﻿# 11:51 AM 5/6/2019 Sync-AD():moved from tsksid-incl-ServerApp.ps1
 #*------v Function Sync-AD v------
 Function Sync-AD { 
-    # let's you trigger a replication between DCs. This function needs further tweaks for re-usability
-    # from dsoldow's https://github.com/dsolodow/IndyPoSH/blob/master/Profile.ps1
+    <#
+    .SYNOPSIS
+    Triggers a replication between Domain Controllers (DCs).
+    .NOTES
+    Version: 1.0.0
+    Author: [Your Name]
+    CreatedDate: [Creation Date]
+    FileName: Sync-AD.ps1
+    Source: Adapted from dsoldow's script at https://github.com/dsolodow/IndyPoSH/blob/master/Profile.ps1
+    .DESCRIPTION
+    The `Sync-AD` function allows you to trigger a replication between specified Domain Controllers (DCs). This function can be customized further for reusability. It uses the `repadmin` tool to perform the replication.
+    .PARAMETER DestinationDC
+    The destination Domain Controller for the replication. Defaults to 'centralDC'.
+    .PARAMETER SourceDC
+    The source Domain Controller for the replication. Defaults to 'localDC'.
+    .PARAMETER DirectoryPartition
+    The directory partition to replicate. Defaults to 'YourDomainName'.
+    .EXAMPLE
+    PS C:\> Sync-AD -DestinationDC 'centralDC' -SourceDC 'localDC' -DirectoryPartition 'YourDomainName'
+    This command triggers a replication from 'localDC' to 'centralDC' for the specified directory partition.
+    .LINK
+    https://github.com/dsolodow/IndyPoSH/blob/master/Profile.ps1
+    #>
     [CmdletBinding()]
     Param (
     [parameter(Mandatory = $false,Position=0)] [String]$DestinationDC = 'centralDC',
